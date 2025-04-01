0987b773850062581a52f31e0abf6e71e216b6d7
Jason <jasonbohio@gmail.com>
2025-04-01 16:23:49 -0400

    nothing invoice

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -7,7 +7,7 @@ const Payment = ({ onPaymentSuccess }) => {
         throw new Error("WebLN provider not found. Please install a WebLN-compatible wallet.");
       }
 
-      const invoice = "lnbc1u1pn7tv7csp5tuq3f2kusf67yku3g59mjge0h3wv37w4jg8ujs93r3y3jq07aw0spp5sjgu0nl9enylkl4e46vflh8zj5s5322uk04ya0e3guxnp3qfp34shp5uwcvgs5clswpfxhm7nyfjmaeysn6us0yvjdexn9yjkv3k7zjhp2sxq9z0rgqcqpnrzjqvjt5gujufdl7a4t6zcl0e948d0c92jnlhwrgxds2xmvsqwmnc3ywrf67vqqnvsqqqqqqqqqqqqqqzsq2q9qxpqysgq9lswwzkrm2kfph4ulvu3tht46ydrdvaajk2uht8al0xyegx35gj5ux2427ekvjjtyct0zpd344e0th8vzqyw3pacc0fnzxz9edhac2cqprxfhu";
+      const invoice = "";
       console.log("Requesting payment...");
       const response = await window.webln.sendPayment(invoice);
       console.log("Payment response:", response);
