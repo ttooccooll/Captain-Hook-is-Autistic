d2f8a6ba60f1f478287b285ff170f29adca58694
Jason <jasonbohio@gmail.com>
2025-04-02 21:08:22 -0400

    credits

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -17,11 +17,16 @@ const Stats = ({ score, totalQuestions, onPlayAgain, gameCount = 0, count = '', 
 
   return (
     <div>
-      <p className='gameover'>Your most recent score: {score}/{totalQuestions}</p>
-      <p className='gameover'>You've played {gameCount} games.</p>
-      <p className='gameover'>Your average score: {averageScore.toFixed(2)}/10</p>
-      <p className='gameover'>Would you like to play again?</p>
+      <p className='over'>Your most recent score: {score}/{totalQuestions}</p>
+      <p className='over'>You've played {gameCount} games.</p>
+      <p className='over'>Your average score: {averageScore.toFixed(2)}/10</p>
+      <p className='over'>Would you like to play again?</p>
       <button onClick={handlePlayAgainClick}></button>
+      <div className='credits'>
+        <p>Concept by Herzl Tuckman</p>
+        <p>Game mechanics and bitcoin shit by <a href="https://stacker.news/jasonb" target="blank" >jasonb</a></p>
+        <p>Questions by <a href="https://stacker.news/jasonb" target="blank" >jasonb</a> and <a href="https://stacker.news/dagny761" target="blank" >dagny761</a></p>
+      </div>
     </div>
   );
 };
